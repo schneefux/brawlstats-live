96dfc2e4cc21479a21619d94355be45b4b2b1b5f
schneefux <schneefux+commit@schneefux.xyz>
2018-12-18 20:11:52 +0000

    Move twitch logic to its own file

--- main.py
+++ main.py
@@ -2,77 +2,23 @@
 # based on https://gist.github.com/kscottz/242176c5bdb282b0a327
 # using streamlink instead of livestreamer https://streamlink.github.io/api_guide.html
 
-import os
 import cv2
 import time
-import requests
-import streamlink
-from classifier import Classifier
 import config
+from classifier import Classifier
+from stream import TwitchStream
 
-TWITCH_HEADERS = { "Client-ID": config.client_id }
-
-stream_resolution_p = "{}p".format(config.stream_resolution)
-
-#r = requests.get("https://api.twitch.tv/helix/games?name=Brawl+Stars",
-#                 headers=TWITCH_HEADERS)
-#twitch_game_id = r.json()["data"][0]["id"]
-twitch_game_id = "497497" # Brawl Stars
-
-r = requests.get("https://api.twitch.tv/helix/streams" +
-                 "?first=10&language=en&game_id=" + twitch_game_id,
-                 headers=TWITCH_HEADERS)
-for channel_data in r.json()["data"]:
-    channel = channel_data["user_name"]
-    buffer_file = "/tmp/" + channel + ".mpg"
-
-    # get Twitch stream
-    streams = streamlink.streams("https://www.twitch.tv/" + channel)
-    if stream_resolution_p not in streams:
-        # try next
-        continue
-
-    print("watching {}'s stream".format(channel))
-    # should be same as template resolution!
-    stream = streams[stream_resolution_p].open()
-    break
-
-buffer = open(buffer_file, "wb")
-print("buffering")
-buffer.write(stream.read(-1))
-
-def get_last_frame():
-    '''
-    Skip all frames from the stream and return the last.
-    '''
-    # TODO this is quite an ugly workaround
-    # might want to try this instead https://github.com/DanielTea/rage-analytics/blob/8e20121794478bda043df4d903aa8709f3ac79fc/engine/realtime_VideoStreamer.py
-    last_frame = None
-    while True:
-        # stream buffer -> file buffer
-        buffer.write(stream.read(-1))
-        while True:
-            # empty file buffer until no data
-            _, frame = cap.read()
-            if frame is None:
-                # TODO suppress errors on console
-                break
-            last_frame = frame
-        if last_frame is not None:
-            return last_frame
-        print("buffering")
-
-
-cap = cv2.VideoCapture(buffer_file)
+stream = TwitchStream(
+    config.stream_resolution, config.client_id)
 classifier = Classifier(config.stream_resolution)
-classifier.load_templates("templates/*.png",
-                          config.template_resolution)
+classifier.load_templates(
+    "templates/*.png", config.template_resolution)
 
 while True:
-    frame = get_last_frame()
+    frame = stream.get_frame()
     cv2.imshow("frame", frame)
 
-    matching_template_name = classifier.classify_image(frame, channel)
+    matching_template_name = classifier.classify_image(frame)
     if matching_template_name is not None:
         print("current frame shows {}!"
               .format(matching_template_name))
@@ -87,7 +33,4 @@ while True:
         filename = channel + "_" + str(int(time.time())) + ".png"
         cv2.imwrite(filename, frame)
 
-buffer.close()
-stream.close()
 cv2.destroyAllWindows()
-os.remove(buffer_file)
---
+++ stream.py
@@ -0,0 +1,75 @@
+import os
+import cv2
+import requests
+import streamlink
+from classifier import Classifier
+
+class StreamSource(object):
+    def get_frame(self):
+        pass
+
+
+class TwitchStream(StreamSource):
+    _twitch_headers = None
+    _http_stream    = None
+    _file_stream    = None
+    _stream         = None
+
+    def __init__(self, stream_resolution, twitch_client_id):
+        # TODO close streams later
+        self._twitch_headers = { "Client-ID": twitch_client_id }
+        self._resolution_p = "{}p".format(stream_resolution)
+
+        self._http_stream, buffer_file = self._get_stream()
+        self._file_stream = open(buffer_file, "wb")
+        self._flush()
+        self._stream = cv2.VideoCapture(buffer_file)
+
+    def _get_game_id(self):
+        #r = requests.get("https://api.twitch.tv/helix/games?name=Brawl+Stars",
+        #                 headers=self._twitch_headers)
+        #twitch_game_id = r.json()["data"][0]["id"]
+        twitch_game_id = "497497" # Brawl Stars
+        return twitch_game_id
+
+    def _get_twitch_channel_names(self, game_id):
+        r = requests.get("https://api.twitch.tv/helix/streams" +
+                         "?first=10&language=en&game_id=" + game_id,
+                         headers=self._twitch_headers)
+        return [data["user_name"] for data in r.json()["data"]]
+
+    def _get_stream(self):
+        channels = self._get_twitch_channel_names(self._get_game_id())
+        for channel in channels:
+            # get Twitch stream
+            streams = streamlink.streams(
+                "https://www.twitch.tv/" + channel)
+            if self._resolution_p in streams:
+                break
+
+        buffer_file = "/tmp/" + channel + ".mpg"
+        stream = streams[self._resolution_p].open()
+        return stream, buffer_file
+
+    def _flush(self):
+        '''
+        Write data from http buffer to file buffer.
+        '''
+        self._file_stream.write(self._http_stream.read(-1))
+
+    def get_frame(self):
+        '''
+        Skip all frames from the stream and return the last.
+        '''
+        # TODO this is quite an ugly workaround
+        # might want to try this instead https://github.com/DanielTea/rage-analytics/blob/8e20121794478bda043df4d903aa8709f3ac79fc/engine/realtime_VideoStreamer.py
+        last_frame = None
+        self._flush()
+        while True:
+            # empty file buffer until no data
+            _, frame = self._stream.read()
+            if frame is None:
+                # TODO suppress errors on console
+                break
+            last_frame = frame
+        return last_frame
