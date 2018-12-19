f8cd534380a3ccf132067fa854ba1ca52ee64759
hidotp <janfasel@gmx.net>
2018-12-19 13:20:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ import time
 import config
 from classifier import Classifier
 from stream import TwitchStream
+from threading import Thread
+
+class backgroundThread(Thread):
+    def __init__(self):
+        super(backgroundThread, self).__init__()
+        self.exitPressed = False
+        self.spacePressed = False
+    def run(self):
+        while not self.exitPressed:
+            # release and check for ESC
+            key = 0xFF
+            if key == 27:
+            # ESC: quit
+                self.exitPressed = True
+            if key == 32:
+            # space: screenshot
+                self.spacePressed = True
+                
 
 stream = TwitchStream(
     config.stream_resolution, config.client_id)
@@ -17,25 +35,23 @@ screen_classifier.load_templates(
 post_match_classifier.load_templates(
     "templates/post_match/*.png", config.template_resolution)
 
-while True:
+running = True
+thread = backgroundThread()
+thread.start()
+
+while running:
     frame = stream.get_frame()
     cv2.imshow("frame", frame)
-
     screen = screen_classifier.classify(frame)
+    #running = thread.exitPressed
     if screen is not None:
         post_match_classifier.scale_factor = screen_classifier.scale_factor
-        match_result = post_match_classifier.classify(frame)
+        match_result = post_match_classifier.classify_image(frame)
         print("current frame shows screen {} with {}!"
               .format(screen, match_result))
-
-    # release and check for ESC
-    key = 0xFF & cv2.waitKey(1)
-    if key == 27:
-        # ESC: quit
-        break
-    if key == 32:
-        # space: screenshot
+    if thread.spacePressed:
         filename = "{}_{}.png".format(stream.channel, int(time.time()))
         cv2.imwrite(filename, frame)
+        thread.spacePressed = False
 
 cv2.destroyAllWindows()
