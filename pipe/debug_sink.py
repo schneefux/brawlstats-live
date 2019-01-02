281a952bdc6ff53782636178cbad6b12cf716160
schneefux <schneefux+commit@schneefux.xyz>
2019-01-02 10:57:04 +0000

    Debug pipe: Store successful classifications for further analysis

--- pipe/debug_sink.py
+++ pipe/debug_sink.py
@@ -1,3 +1,5 @@
+import os
+import cv2
 import time
 import logging
 
@@ -17,6 +19,19 @@ class DebugSink(Sink):
         self._durations.append(duration)
         if len(self._durations) > 10:
             self._durations = self._durations[1:]
+
+        if state.current_screen is not None:
+            screen_box = state.stream_config.screen_box
+            cut_frame = frame[screen_box[0][1]:screen_box[1][1],
+                              screen_box[0][0]:screen_box[1][0]]
+            path = "dataset/{}/".format(
+                    state.current_screen.name.lower())
+            if not os.path.exists(path):
+                os.makedirs(path)
+            cv2.imwrite("{}/{}_{}.jpg".format(
+                path,
+                state.stream_config.channel,
+                int(state.timestamp)), cut_frame)
 
         logging.debug(
             "screen: %s (last: %s, result: %s), " +
