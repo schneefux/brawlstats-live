6e4202448023f549a9b112a8baee1a058502b5e2
schneefux <schneefux+commit@schneefux.xyz>
2019-01-11 22:15:55 +0100

    Screen convnet: Train a better and faster model

--- model/screen/train.py
+++ model/screen/train.py
@@ -26,11 +26,6 @@ model.add(BatchNormalization(axis=channel_dim))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(BatchNormalization(axis=channel_dim))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation("relu"))
