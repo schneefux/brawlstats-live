3618426ac61afa2767f1a737d56e3f0febee3ffa
schneefux <schneefux+commit@schneefux.xyz>
2019-01-16 19:45:00 +0100

    Reset gems on play again screen

--- pipe/gembar_pipe.py
+++ pipe/gembar_pipe.py
@@ -8,8 +8,14 @@ class GembarPipe(Pipe):
         self._matcher_red = RedGembarMatcher()
 
     def process(self, frame, state):
-        if state.stream_config.screen_box is None \
-            or state.screen != Screen.GEMGRAB_INGAME:
+        if state.stream_config.screen_box is None:
+            return {}
+        if state.screen == Screen.PLAY_AGAIN:
+            return {
+                "blue_gems": 0,
+                "red_gems": 0,
+            }
+        if state.screen != Screen.GEMGRAB_INGAME:
             return {}
 
         blue_gems = int(10 * self._matcher_blue.classify(
--- pipe/test/test_gembar_pipe.py
+++ pipe/test/test_gembar_pipe.py
@@ -22,6 +22,17 @@ def test_should_set_state():
     }
 
 
+def test_should_reset_state():
+    state = GameState(stream_config=stream_config,
+                      screen=Screen.PLAY_AGAIN)
+    pipe = GembarPipe()
+    changes = pipe.process(None, state)
+    assert changes == {
+        "blue_gems": 0,
+        "red_gems": 0,
+    }
+
+
 def test_should_ignore_wrong_screen():
     state = GameState(stream_config=stream_config,
                       screen=Screen.MAIN_MENU)
